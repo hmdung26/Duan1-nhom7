176709d7ab4dbaa3ae1ba9296189d6d4940828d8
hmdun <hmdung26@gmail.com>
2023-03-16 10:29:33 +0700

    test2

--- duan1_nhom7/App.js
+++ duan1_nhom7/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Xin chào</Text>
+      <Text>Xin chào! tôi là Dũng</Text>
       <StatusBar style="auto" />
     </View>
   );
